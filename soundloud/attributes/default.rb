b15c892d93af494c8460e77f6c1d60570732c49f
Edan Lewis <edan@lewisarts.com>
2018-04-12 19:47:19 -0700

    s3 info

--- soundloud/attributes/default.rb
+++ soundloud/attributes/default.rb
@@ -14,7 +14,11 @@ default[:soundloud][:environment] = {
   "SECRET_KEY_BASE": node[:soundloud][:secret_key_base],
   "DATABASE_URL": node[:soundloud][:database_url],
   "RAILS_ENV": node[:soundloud][:rails_env],
-  "RAILS_LOG_TO_STDOUT": node[:soundloud][:log_to_stdout]
+  "RAILS_LOG_TO_STDOUT": node[:soundloud][:log_to_stdout],
+  "S3_BUCKET": node[:soundloud][:s3_bucket],
+  "S3_REGION": node[:soundloud][:s3_region],
+  "S3_ACCESS_REGION": node[:soundloud][:s3_access_region],
+  "S3_SECRET_ACCESS_KEY": node[:soundloud][:s3_secret_access_key]
 }
 
 default[:soundloud][:start_cmd] = "unicorn -E production -c /opt/unicorn.rb"
